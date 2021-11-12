9108c98ca2a315d59d1cdafdb1f0454de64e77d4
Hoang2k <dinhvanhoang472000@gmail.com>
2021-11-12 08:28:13 +0700

    Initial commit

---
+++ todo-jquery/assets/js/main.js
@@ -0,0 +1,43 @@
+
+$(document).on('click','.btn-add',function() {
+  var input=$(this).siblings('.todo-input').val();
+  var todoList=`
+   <ul>
+   <li class="todo-item">
+   <div class="task-name">`+input+`</div>
+       
+   <a> <i class="fas fa-edit"></i></a> 
+   <a ><i class="fas fa-trash-alt"></i></a>
+   </li>
+   </ul>
+  `;
+  
+  if($.trim(input)==''){
+    alert("Vui lòng nhập tên công việc");
+  }
+  else {
+    $(this).parents('.content').find('#result').append(todoList);
+  }
+  $(this).siblings('.todo-input').val('');
+});
+//Edit
+
+$(document).on('click','.fa-edit',function() {
+  var listText= $(this).closest('.todo-item').find('.task-name').html();
+
+  var input=$('.todo-input');
+  input.val(listText);
+
+})
+// remove todo list script
+$(document).on('click','.fa-trash-alt',function(){
+  if(confirm("Bạn Có muốn xóa công việc này ! ")){
+    $(this).closest('.todo-item').remove();
+  }
+  
+ })
+ 
+ //Mark ToDo
+ $(document).on('click','.task-name',function(){
+  $(this).toggleClass('done');
+ });
